85df57d0746d3b425a7e2557a03c0c4241860680
Sarunas Janickas <sarunas.janickas@gmail.com>
2020-09-20 14:17:04 +0300

    RoomEntity, RoomTypeEntity, Size enum, RoomCondition enum

--- src/main/java/lt/sdacademy/famtrip/models/entities/RoomTypeEntity.java
+++ src/main/java/lt/sdacademy/famtrip/models/entities/RoomTypeEntity.java
@@ -10,4 +10,12 @@ public class RoomTypeEntity extends AbstractEntity {
 
     @Column(name = "title", length = 50, unique = true, nullable = false)
     private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
