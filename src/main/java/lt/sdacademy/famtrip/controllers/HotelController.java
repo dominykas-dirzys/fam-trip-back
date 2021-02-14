f5b6a6548cf048cd432c313c335d35cc0a6e31ee
Sarunas Janickas <sarunas.janickas@gmail.com>
2021-02-14 09:56:26 +0200

    HotelController skeletas

---
+++ src/main/java/lt/sdacademy/famtrip/controllers/HotelController.java
@@ -0,0 +1,33 @@
+package lt.sdacademy.famtrip.controllers;
+
+import lt.sdacademy.famtrip.services.HotelService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/hotels")
+public class HotelController {
+
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping
+//    TODO
+
+    @GetMapping("{id}")
+//    TODO
+
+    @PostMapping
+//    TODO
+
+    @DeleteMapping
+//    TODO
+}
